7ef734932c7a8e0f46b090d9b562d9ad07b49acd
Limjaewoo <ljw3944@naver.com>
2023-02-20 01:16:46 +0900

    Mybatis

---
+++ src/main/java/hello/itemservice/repository/mybatis/MyBatisItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor
+public class MyBatisItemRepository implements ItemRepository {
+
+    private final ItemMapper itemMapper;
+
+    @Override
+    public Item save(Item item) {
+        //itemMapper는 인터페이스를 주입받은게 아니라 마이바티스가 ItemMapper를 프록시로 구현체를 생성한 후에 주입받은 것이다.
+        log.info("itemMapper class={}",itemMapper.getClass());
+        itemMapper.save(item);
+        return item;
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        itemMapper.update(itemId,updateParam);
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return itemMapper.findById(id);
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        return itemMapper.findAll(cond);
+    }
+}
